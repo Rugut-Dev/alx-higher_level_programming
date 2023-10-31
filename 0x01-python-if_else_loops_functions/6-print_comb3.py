71156c6fb57aa14b1e6410fbcdeb0ec4f6328f7a
Kelvin Rugut <krugut36@gmail.com>
2023-10-31 15:32:59 -0700

    Combinations

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i // 10 < i % 10:
+    if i // 10 < i % 10 and i < 89:
         print("{:02d}".format(i), end=", ")
+    elif i >= 89 and i // 10 < i % 10:
+        print("{:02d}".format(i))
     else:
         pass
-print()
