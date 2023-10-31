d75a5b84178ed64d8f6150420b0c89cba40d71fe
Kelvin Rugut <krugut36@gmail.com>
2023-10-31 14:25:28 -0700

    .upper

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    uppercase_text = ""
+    for i in str:
+        if 'a' <= i <= 'z':
+            uppercase_i = chr(ord(i) - 32)
+            uppercase_text += uppercase_i
+        else:
+            uppercase_text += i
+    print(uppercase_text)
