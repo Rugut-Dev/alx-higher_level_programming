61b9a8a585139a8360dad3fdf79e4a9b0d1acbca
Kelvin Rugut <krugut36@gmail.com>
2023-10-31 13:49:52 -0700

    letter case

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,5 +2,5 @@
 def islower(c):
     if 'a' <= c <= 'z' and len(c) != 0:
         return True
-    elif 'A' <= c <= 'Z'and len(c) != 0:
+    elif 'A' <= c <= 'Z' and len(c) != 0:
         return False
