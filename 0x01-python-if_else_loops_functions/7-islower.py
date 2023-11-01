099c52c2c481f7c6a060bdc671ae14b74bb04de4
Kelvin Rugut <krugut36@gmail.com>
2023-11-01 01:50:52 -0700

    Lowercase checker

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -3,5 +3,5 @@ def islower(c):
     for i in c:
         if ord(i) >= 97 and ord(i) <= 122:
             return True
-        else:
+        elif ord(i) >= 65 and ord(i) <= 90:
             return False
