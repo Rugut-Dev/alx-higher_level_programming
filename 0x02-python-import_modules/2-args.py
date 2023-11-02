b9ef35784ecb9d3ab21c9994039f3062c8b12e4e
Kelvin Rugut <krugut36@gmail.com>
2023-11-02 10:13:42 -0700

    argv listing

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    if len(argv) == 1:
+        print("0 arguments.")
+    elif len(argv) == 2:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+    else:
+        print("{} arguments:".format(len(argv) - 1))
+        for i in range(len(argv)):
+            if i == 0:
+                pass
+            else:
+                print("{}: {}".format(i, argv[i]))
