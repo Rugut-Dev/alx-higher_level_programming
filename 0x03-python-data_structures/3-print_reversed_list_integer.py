5dd7975b42b80a742674a4b79f293a2df62a7caa
Kelvin Rugut <krugut36@gmail.com>
2023-11-06 20:23:08 +0300

    Print in reverse without casting

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,7 @@
 
 def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
-    for i in range(5):
+    for i in range(len(my_list)):
         print("{:d}".format(my_list[i]))
 
 #    for i in my_list[::-1]:
