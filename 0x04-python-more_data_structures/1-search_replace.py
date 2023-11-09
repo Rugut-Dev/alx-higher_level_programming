7611877955a20f321ca5afec2b4796d82ed5615f
Kelvin Rugut <krugut36@gmail.com>
2023-11-09 13:04:34 +0300

    Replace elem in list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    list_copy = my_list.copy()
+    cplist = my_list.copy()
 
-    for i in list_copy:
-        new_list = list(map(lambda i: replace if i == search else i, list_copy))
+    for i in cplist:
+        new_list = list(map(lambda i: replace if i == search else i, cplist))
 
     return new_list
