00d71f6fb88f939c4adc9b66d5baecee1ed47e20
Kelvin Rugut <krugut36@gmail.com>
2023-11-09 12:47:05 +0300

    Replace elem in list

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    for i in my_list:
+        new_list = list(map(lambda i: replace if i == search else i, my_list))
+
+    return new_list
