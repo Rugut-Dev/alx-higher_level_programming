f89f60220b359add83bea4dc882a88e9821cae55
Kelvin Rugut <krugut36@gmail.com>
2023-11-10 16:19:12 +0300

    Search list and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    if my_list:
-        for i in my_list:
-            n_list = list(map(lambda i: replace if i == search else i, my_list))
+    for i in my_list:
+        n_list = list(map(lambda i: replace if i == search else i, my_list))
 
     return n_list
