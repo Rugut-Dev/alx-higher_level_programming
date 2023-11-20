b12088da422fb00990a87c508163351c02f0318a
Kelvin Rugut <krugut36@gmail.com>
2023-11-20 13:55:15 +0300

    Safe print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,7 +8,7 @@ def safe_print_list(my_list=[], x=0):
         for i in new_list:
             print("{}".format(i), end="")
             num += 1
-    except:
+    except Exception:
         print("Error Occured\n")
     finally:
         print()
