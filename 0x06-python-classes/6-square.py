6798b5adb29779d932737960800a90550f4e72fa
Kelvin Rugut <krugut36@gmail.com>
2023-11-21 21:45:17 +0300

    Prints square in position def by tuple

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square by: (based on 5-square.py)"""
+
+
+class Square:
+    """Print Square given coordinates"""
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.value = size
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.value = __position
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return
+        else:
+            if self.__position[1] > 0:
+                for pos in range(self.__position[1]):
+                    print()
+            for i in range(self.__size):
+                if self.__position[0] > 0:
+                    for pos in range(self.__position[0]):
+                        print(" ", end="")
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
