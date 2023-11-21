c7639f266235d6bceb090f9700582bc3e9c394bb
Kelvin Rugut <krugut36@gmail.com>
2023-11-21 18:32:01 +0300

    Public instance Method

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -2,7 +2,6 @@
 """ a class Square that defines a square by: (based on 2-square.py)"""
 
 
-
 class Square:
     """Square that defines a square"""
     def __init__(self, size=0):
@@ -11,7 +10,6 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-
 
     def area(self):
         """returns square of size"""
