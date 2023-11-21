d833bba563aa84fdcd7ec2723592c431f8fe5c32
Kelvin Rugut <krugut36@gmail.com>
2023-11-21 19:49:03 +0300

    Getter and setter properties

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square by: (based on 3-square.py)"""
+
+
+class Square:
+    """class Square defining a square, privately instantiating size"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    """getter for size"""
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter function with error checking"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """returns area of a square given size"""
+        return self.__size * self.__size
