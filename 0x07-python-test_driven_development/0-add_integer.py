629fa4648582f884e485d09106815ee8b6ffd354
Kelvin Rugut <krugut36@gmail.com>
2023-11-24 18:12:58 +0300

    Addition testcase

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,6 +5,7 @@ This module provides a function to add two integers or floats.
 It checks the types of input parameters and returns their sum as an integer.
 """
 
+
 def add_integer(a, b=98):
     """Returns an integer: addition of a and b"""
     if type(a) != int and type(a) != float:
