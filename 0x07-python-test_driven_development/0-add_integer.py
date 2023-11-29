1d2fe5a2d8ff7ebf55a260ac851c0254ce0c0e98
Kelvin Rugut <krugut36@gmail.com>
2023-11-29 10:22:48 +0300

    Addition of int testcases

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,9 +8,9 @@ It checks the types of input parameters and returns their sum as an integer.
 
 def add_integer(a, b=98):
     """Returns an integer: addition of a and b"""
-    if type(a) != int and type(a) != float:
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    elif type(b) != int and type(b) != float:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
