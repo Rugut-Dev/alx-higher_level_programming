34082ffb46ce1ea823d444680342f0f4e2af1ee1
Kelvin Rugut <krugut36@gmail.com>
2023-12-04 20:43:45 +0300

    inheritance and using super()

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Based on 6-base_geometry"""
+
+
+class BaseGeometry:
+    """Raises an exeception that area() is not implemented
+validates value
+
+"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        super().integer_validator("width", self.__width)
+        super().integer_validator("height", self.__height)
