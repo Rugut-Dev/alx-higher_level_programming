8bc2bc28c3382d6b5bd44cd2725e39d6cc2cf182
Kelvin Rugut <krugut36@gmail.com>
2023-12-04 22:53:12 +0300

    My list inherits

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,9 +4,6 @@
 
 class MyList(list):
     """returns sorted list"""
-    def __init__(self):
-        super().__init__()
-
     def print_sorted(self):
         sorted_list = sorted(self)
         print(sorted_list)
