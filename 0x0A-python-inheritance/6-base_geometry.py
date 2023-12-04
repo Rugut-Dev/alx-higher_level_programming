d722892af918434813b224ceddd3a78267692e57
Kelvin Rugut <krugut36@gmail.com>
2023-12-04 19:51:42 +0300

    Raise exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Based on 5-base_geometry"""
+
+
+class BaseGeometry:
+    """Raises an exeception that area() is not implemented"""
+    def area(self):
+        raise Exception("area() is not implemented")
