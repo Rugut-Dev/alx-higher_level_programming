211068cf338ef728e1c2668c008379f2bdd3874d
Kelvin Rugut <krugut36@gmail.com>
2023-12-04 19:19:36 +0300

    use of isintance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,7 +4,4 @@
 
 def is_same_class(obj, a_class):
     """function checking isinstance"""
-    if isinstance(obj, a_class):
-        return True
-    else:
-        return False
+    return type(obj) == a_class
