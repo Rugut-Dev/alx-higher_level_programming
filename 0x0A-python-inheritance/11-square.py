f89236053750ee0bb3ef5cdbf00eef76a4c3f813
Kelvin Rugut <krugut36@gmail.com>
2023-12-04 22:45:16 +0300

    Prints square

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,44 +1,20 @@
 #!/usr/bin/python3
 """Based on 6-base_geometry"""
 
-
-class BaseGeometry:
-    """Raises an exeception that area() is not implemented
-validates value
-
-"""
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) != int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """inherits from BaseGeometry"""
-    def __init__(self, width, height):
-        super().__init__()
-        self.__width = width
-        self.__height = height
-        self.integer_validator("width", self.__width)
-        self.integer_validator("height", self.__height)
-
-    def area(self):
-        return self.__width * self.__height
-
-    def __str__(self):
-        return f"[Rectangle] {self.__width}/{self.__height}"
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
     """inherits from square"""
     def __init__(self, size):
-        super().__init__(size, size)
+        self.integer_validator("size", size)
         self.__size = size
-        self.integer_validator("size", self.__size)
+        super().__init__(size, size)
+
+    def area(self):
+        """returns area of square"""
+        return self.__size ** 2
 
     def __str__(self):
-        return f"[Square] {self.__size}/{self.__size}"
+        """Informal string rep"""
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
