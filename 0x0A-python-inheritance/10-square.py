92854a494eb21f342ab970dcc52b34b490d24592
Kelvin Rugut <krugut36@gmail.com>
2023-12-04 22:00:13 +0300

    Inheritance anaccessing methods

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Based on 6-base_geometry"""
+
+
+class BaseGeometry:
+    """Raises an exeception that area() is not implemented
+validates value
+
+"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        super().__init__()
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+
+class Square(Rectangle):
+    """inherits from square"""
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.__size = size
+        self.integer_validator("size", self.__size)
+
+    def __str__(self):
+        return f"[Rectangle] {self.__size}/{self.__size}"
