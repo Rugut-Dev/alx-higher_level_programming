b67202d0b9b73665725c4b95df9f5480b77be7dd
Kelvin Rugut <krugut36@gmail.com>
2023-12-05 15:32:01 +0300

    Print lines from a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Prints lines of a file"""
+
+
+def read_file(filename=""):
+    with open(filename, mode='r',  encoding="utf-8") as f:
+        for line in f:
+            print(line, end='')
