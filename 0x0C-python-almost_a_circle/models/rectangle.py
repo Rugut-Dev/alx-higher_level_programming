b51b89d11c3f8d48bcc4263b45c6559b76bc484a
Kelvin Rugut <krugut36@gmail.com>
2023-12-08 18:07:18 +0300

    Update #1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -81,14 +81,10 @@ class Rectangle(Base):
             f" {self.x}/{self.y} - {self.width}/{self.height}"
             )
 
-    def update(self, *args):
-        if len(args) > 0:
-            self.id = args[0]
-        if len(args) > 1:
-            self.width = args[1]
-        if len(args) > 2:
-            self.height = args[2]
-        if len(args) > 3:
-            self.x = args[3]
-        if len(args) > 4:
-            self.y = args[4]
+    def update(self, *args, **kwargs):
+        if args:
+            attrs = ['id', 'width', 'height', 'x', 'y']
+            for i, arg in enumerate(args[:5]):
+                setattr(self, attrs[i], arg)
+        for key, val in kwargs.items():
+            setattr(self, key, val)
