07b2f246327d9506c47a534fa9ee9407d76336fc
Kelvin Rugut <krugut36@gmail.com>
2024-01-16 18:13:48 +0300

    Full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,7 +2,7 @@
 -- second_table description:
 --      id INT
 --      name VARCHAR(256)
--- score INT
+-- 	score INT
 -- The database name will be passed as an argument to the mysql command
 -- If the table second_table already exists, your script should not fail
 -- You are not allowed to use the SELECT and SHOW statements
@@ -13,7 +13,7 @@
 -- 	id = 4, name = “George”, score = 8
 
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(20), score INT);
-INSERT INTO second_table VALUES (1, 'John', 10)
-INSERT INTO second_table VALUES (2, 'Alex', 3),
-INSERT INTO second_table VALUES (3, 'Bob', 14),
+INSERT INTO second_table VALUES (1, 'John', 10);
+INSERT INTO second_table VALUES (2, 'Alex', 3);
+INSERT INTO second_table VALUES (3, 'Bob', 14);
 INSERT INTO second_table VALUES (4, 'George', 8);
