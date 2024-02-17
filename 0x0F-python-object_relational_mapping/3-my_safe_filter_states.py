47cb9d4109ff0b227b117f436372da293f146f61
Kelvin Rugut <krugut36@gmail.com>
2024-02-18 00:21:09 +0300

    SQL Injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+script thaT lists all states from the
+database `hbtn_0e_0_usa`.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        port=3306,
+        db=argv[3],
+        passwd="password"
+        )
+
+    with db.cursor() as csr:
+        csr.execute("""
+            SELECT * FROM states WHERE name LIKE BINARY %(name)s \
+ORDER BY states.id ASC""", {'name': argv[4]})
+    rows = csr.fetchall()
+
+    for row in rows:
+        print(row)
