8d074ebe3ee06fda746570f0b2a997303ed64efc
Kelvin Rugut <krugut36@gmail.com>
2024-03-12 13:07:21 +0300

    Whats my status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+import requests
+
+
+def fetch_status():
+    response = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
+
+if __name__ == "__main__":
+    fetch_status()
